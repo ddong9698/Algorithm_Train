1676dbe52c36141e2868ef455a1e4f4ef156e085
ddong9698 <ddong9698@naver.com>
2021-04-18 13:46:45 +0000

    test

---
+++ Nadocoding_basic/practice1.py
@@ -0,0 +1,13 @@
+# 변수를 사용한
+
+animal = "강아지"
+name = "연탄이"
+age = 4 #정수형
+hobby = "산책"
+is_adult = age >= 3
+
+print("우리집 " + animal + "의 이름은 " +name + "예요.")
+print(name + "는 " + str(age) + "살이며, " + hobby + "를 좋아해요")
+print(name + "는 어른일까요? " + str(is_adult))
+# ctrl + /
+
