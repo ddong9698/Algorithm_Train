8c152e2118a144e05650454b9be8dcb676cedcaa
ddong9698 <ddong9698@naver.com>
2021-04-18 13:47:47 +0000

    test

--- Nadocoding_basic/practice1.py
+++ Nadocoding_basic/practice1.py
@@ -10,4 +10,7 @@ print("우리집 " + animal + "의 이름은 " +name + "예요.")
 print(name + "는 " + str(age) + "살이며, " + hobby + "를 좋아해요")
 print(name + "는 어른일까요? " + str(is_adult))
 # ctrl + /
+#########################
 
+print(1 != 3) # 1은 앞에 3과 같지 않다. True
+print(not(1 != 3)) # false
--- beakjoon/12.py
+++ beakjoon/12.py
@@ -1,1 +1,2 @@
-print('hi')
+# print('hi')
+print(5 > 10)
