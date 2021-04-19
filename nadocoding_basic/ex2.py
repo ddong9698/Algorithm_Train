4bf5619168bf51bde9a1cf0647ef70acb7981074
ddong9698 <ddong9698@naver.com>
2021-04-19 14:00:26 +0000

    test

---
+++ nadocoding_basic/ex2.py
@@ -0,0 +1,9 @@
+# print(abs(-5)) # 5
+# print(pow(4, 2)) # 4^2
+# print(max(5, 12))
+# print(min(5, 12))
+
+from math import *
+print(floor(4.99)) #내림
+print(ceil(3.14)) #올림
+print(sqrt(16))
---
+++ nadocoding_basic/ex3.py
@@ -0,0 +1,7 @@
+from random import *
+
+print(random()) # 1미만의 임의의 값 생성
+print(int(randomg() * 10) + 1)
+
+print(randrange(1, 46) # 1~46 미만의 임의의 값
+print(randint(1, 45))
