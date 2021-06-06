8eefbfa2faefed489433fb9f2f21c302f70987ce
ddong9698 <ddong9698@naver.com>
2021-06-06 13:58:16 +0000

    test

--- baekjoon/2562.py
+++ baekjoon/2562.py
@@ -1,5 +1,6 @@
-for i in:
+num_list = []
+for i in range(9):
+    num_list.append(int(input()))
     
-
-print(random)
-print( 3, 29, 38)
+print(max(num_list))
+print(num_list.index(max(num_list))+1)
